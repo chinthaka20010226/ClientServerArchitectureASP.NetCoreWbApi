d261cdce78893603293df9ef76c00d29747269b6
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-14 23:38:06 +0530

    setup the Models

--- Models/Entities/Department.cs
+++ Models/Entities/Department.cs
@@ -1,6 +1,18 @@
-﻿namespace employee_crud_back_.Models.Entities
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace employee_crud_back_.Models.Entities
 {
     public class Department
     {
+        [Key]
+        [Column("id")]
+        [DisplayName("Department ID")]
+        public int Id { get; set; }
+
+        [Column("name"), MaxLength(20)]
+        [DisplayName("DepartmentName")]
+        public required string Name { get; set; }
     }
 }
--- Models/Entities/Employee.cs
+++ Models/Entities/Employee.cs
@@ -1,6 +1,42 @@
-﻿namespace employee_crud_back_.Models.Entities
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace employee_crud_back_.Models.Entities
 {
     public class Employee
     {
+        [Key]
+        [Column("id")]
+        [DisplayName("Employee ID")]
+        public int Id { get; set; }
+
+        [Column("fame"), MaxLength(30)]
+        [DisplayName("Employee First Name")]
+        public required string FName { get; set; }
+
+        [Column("lname"), MaxLength(30)]
+        [DisplayName("Employee Last Name")]
+        public required string LName { get; set; }
+
+        [Column("email"), MaxLength(50)]
+        [DisplayName("Employee Email")]
+        public required string Email { get; set; }
+
+        [Column("dob")]
+        [DisplayName("Employee Date of Birthday")]
+        public required DateOnly DOB { get; set; }
+
+        [Column("salary")]
+        [DisplayName("Employee Salary")]
+        [GreaterThan(0)]
+        public required double Salary { get; set; }
+
+        [Column("department_id")]
+        [DisplayName("Department Id")]
+        [ForeignKey("Department")]
+        public required int DId { get; set; }
+
+        public Department? Department { get; set; }
     }
 }
