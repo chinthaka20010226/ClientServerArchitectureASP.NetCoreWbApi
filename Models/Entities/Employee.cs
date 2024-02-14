00c8102525d9e5ca88496a34ea8c12c43ffe55b8
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-14 23:51:42 +0530

    setup the Validation part

--- Models/Entities/Employee.cs
+++ Models/Entities/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using employee_crud_back_.Models.Validations;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
---
+++ Models/Validations/GreaterThanAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace employee_crud_back_.Models.Validations
+{
+    public class GreaterThanAttribute(int lowerlimit) : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not double salary)
+            {
+                new ValidationResult("Salart type must be double");
+            }
+
+            return salary >= lowerlimit ? ValidationResult.Success : new ValidationResult($"Salary must be greater than or equal to the{lowerlimit}"); 
+        }
+    }
+}
