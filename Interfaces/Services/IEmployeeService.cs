a371cb9b44b22de3679d2db596628dc84026ca39
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-15 02:13:37 +0530

    setup Services Interfaces

--- Interfaces/Services/IEmployeeService.cs
+++ Interfaces/Services/IEmployeeService.cs
@@ -1,6 +1,13 @@
-﻿namespace employee_crud_back_.Interfaces.Services
+﻿using employee_crud_back_.Models.Entities;
+
+namespace employee_crud_back_.Interfaces.Services
 {
     public interface IEmployeeService
     {
+        Task<Employee> GetEmployeeById(int id);
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> InsertEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> DeleteEmployee(int id);
     }
 }
